d2e4415ebbc4e27f9796c58500c222ef2a0cf053
Branislav <banemkd23@hotmail.com>
2019-09-21 14:43:29 +0200

    added mat-card in courses component

--- e-learn-web-app/src/app/modules/course/course.component.ts
+++ e-learn-web-app/src/app/modules/course/course.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-course',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _router: Router) { }
 
   ngOnInit() {
+  }
+
+  public onLanguageArts() {
+    const self = this;
+    self._router.navigate(['language-course']);
   }
 
 }
